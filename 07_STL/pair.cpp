a725ad9975a2539e1d3ddc3501248b2ee7483162
Akash <akashkudecha891@gmail.com>
2026-05-15 20:44:05 +0500

    Add STL pair example

---
+++ 07_STL/pair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    pair<int,string> student;
+
+    student.first = 1;
+    student.second = "Ali";
+
+    cout << student.first << endl;
+    cout << student.second;
+
+    return 0;
+}
