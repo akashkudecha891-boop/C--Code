c3a573166878e701d2a420ad8bc9deaef2970e11
Akash <akashkudecha891@gmail.com>
2026-05-15 20:28:41 +0500

    Add variables and data types example

---
+++ 01_Basics/variables_and_datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float cgpa = 3.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "CGPA: " << cgpa << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
