369aecada197288d3b446b9f263408ba843b3934
Akash <akashkudecha891@gmail.com>
2026-05-15 20:40:31 +0500

    Add linked list example

---
+++ 08_DSA/linked_list.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+int main() {
+    Node* head = new Node(10);
+    cout << head->data;
+
+    return 0;
+}
