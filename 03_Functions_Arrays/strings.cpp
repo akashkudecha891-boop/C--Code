be6eafd1a24d205666ee74ffc238628683e94ecf
Akash <akashkudecha891@gmail.com>
2026-05-15 20:33:15 +0500

    Add strings example

---
+++ 03_Functions_Arrays/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Akash";
+
+    cout << "Name: " << name << endl;
+    cout << "Length: " << name.length();
+
+    return 0;
+}
